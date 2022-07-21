169ea4027bc270840179f3b06a1abae6f5fe8960
Tayven Smith <109565938+TayvenS@users.noreply.github.com>
2022-07-21 16:27:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import time
+
+# Introduction (pre-choices)
+# --------------------------
+
+print(". . .")
+time.sleep(3)
+print(".  .  .")
+time.sleep(5)      
+print("oh... it's you...")
+time.sleep(3)
+print("what... are you just going to sit there?")
+time.sleep(1)
+print(". . .")
 
